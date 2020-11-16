0b5ac18f43737e5f28ad9e53f689d572bf7d676b
Ian F <ian.fiddeman@gmail.com>
2020-11-16 15:06:32 +0000

    arrow functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+// Named function
+function sayHello(greeting, name = "World"){
+    return `${greeting} ${name}!`;
+}
+
+// console.log("sayHello message:", sayHello("Hi", 'Ian'));
+// console.log("sayHello message:", sayHello("Hi"));
+
+// Anonymous function
+var add = function(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
+}
+
+// console.log("1 + 3 with add function:", add(1, 3));
+
+// arrow function with explicit return
+var multiply = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+
+// console.log("multiply 2 by 5:", multiply(2, 5));
+// arrow function single line if single expression 
+var multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+
+
+console.log("multiply 2 by 5:", multiply(2, 5));
